c1e264e324dddf7704a3e642898e7aacaedc7130
Alexander Babai <aliaksandr.babai@gmail.com>
2016-09-19 22:19:58 +0300

    add initial gradle multi-module structure
    * initialize gradle project;
    * create simple projects hierarchy;
    * manage common properties in the root project;
    * manage dependencies between projects;
    * add spring-boot-plugin to the root project;
    * fix .gitignore

---
+++ frontend/src/main/java/com/github/alebabai/tg2vk/Tg2VkApplication.java
@@ -0,0 +1,12 @@
+package com.github.alebabai.tg2vk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tg2VkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tg2VkApplication.class, args);
+	}
+}
