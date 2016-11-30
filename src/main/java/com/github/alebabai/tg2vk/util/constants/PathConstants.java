1ad14dac149b3bdc58ea711fd61724f520fcf254
Alexander Babai <aliaksandr.babai@gmail.com>
2016-11-30 22:36:52 +0300

    use final classes for constants
    instead of interface (cause constant interfaces is anti-pattern)

--- src/main/java/com/github/alebabai/tg2vk/util/constants/PathConstants.java
+++ src/main/java/com/github/alebabai/tg2vk/util/constants/PathConstants.java
@@ -1,16 +1,20 @@
 package com.github.alebabai.tg2vk.util.constants;
 
-public interface PathConstants {
-    String ROOT = "/";
+public final class PathConstants {
+    public static final String ROOT = "/";
 
-    String PAGE_ERROR = "/error";
+    public static final String PAGE_ERROR = "/error";
 
-    String API = "/api";
+    public static final String API = "/api";
 
-    String API_AUTHORIZATION = API + "/auth";
-    String API_LOGIN = API_AUTHORIZATION + "/login";
-    String API_AUTHORIZE = API_AUTHORIZATION + "/authorize";
+    public static final String API_AUTHORIZATION = API + "/auth";
+    public static final String API_LOGIN = API_AUTHORIZATION + "/login";
+    public static final String API_AUTHORIZE = API_AUTHORIZATION + "/authorize";
 
-    String API_TELEGRAM = API + "/telegram";
-    String API_TELEGRAM_FETCH_UPDATES = API_TELEGRAM + "/fetch-updates";
+    public static final String API_TELEGRAM = API + "/telegram";
+    public static final String API_TELEGRAM_FETCH_UPDATES = API_TELEGRAM + "/fetch-updates";
+
+    private PathConstants(){
+        super();
+    }
 }
--- src/main/java/com/github/alebabai/tg2vk/util/constants/VkConstants.java
+++ src/main/java/com/github/alebabai/tg2vk/util/constants/VkConstants.java
@@ -1,36 +1,39 @@
 package com.github.alebabai.tg2vk.util.constants;
 
 
-public interface VkConstants {
-    String VK_URL_OAUTH_BASE = "https://oauth.vk.com";
-    String VK_URL_AUTHORIZE = VK_URL_OAUTH_BASE + "/authorize";
-    String VK_URL_REDIRECT = VK_URL_OAUTH_BASE + "/blank.html";
+public final class VkConstants {
+    public static final String VK_URL_OAUTH_BASE = "https://oauth.vk.com";
+    public static final String VK_URL_AUTHORIZE = VK_URL_OAUTH_BASE + "/authorize";
+    public static final String VK_URL_REDIRECT = VK_URL_OAUTH_BASE + "/blank.html";
 
-    String VK_API_VERSION = "5.59";
-    String VK_RESPONSE_TYPE_CODE = "code";
+    public static final String VK_API_VERSION = "5.59";
+    public static final String VK_RESPONSE_TYPE_CODE = "code";
 
-    String VK_DISPLAY_TYPE_PAGE = "page";
-    String VK_DISPLAY_TYPE_POPUP = "popup";
-    String VK_DISPLAY_TYPE_MOBILE = "mobile";
+    public static final String VK_DISPLAY_TYPE_PAGE = "page";
+    public static final String VK_DISPLAY_TYPE_POPUP = "popup";
+    public static final String VK_DISPLAY_TYPE_MOBILE = "mobile";
 
-    String VK_SCOPE_NOTIFY = "notify";
-    String VK_SCOPE_FRIENDS = "friends";
-    String VK_SCOPE_PHOTOS = "photos";
-    String VK_SCOPE_AUDIO = "audio";
-    String VK_SCOPE_VIDEO = "video";
-    String VK_SCOPE_PAGES = "pages";
-    String VK_SCOPE_NOTES = "notes";
-    String VK_SCOPE_STATUS = "status";
-    String VK_SCOPE_MESSAGES = "messages";
-    String VK_SCOPE_WALL = "wall";
-    String VK_SCOPE_ADS = "ads";
-    String VK_SCOPE_OFFLINE = "offline";
-    String VK_SCOPE_DOCS = "docs";
-    String VK_SCOPE_GROUPS = "groups";
-    String VK_SCOPE_NOTIFICATIONS = "notifications";
-    String VK_SCOPE_STATS = "stats";
-    String VK_SCOPE_EMAIL = "email";
-    String VK_SCOPE_MARKET = "market";
-    String VK_SCOPE_NOHTTPS = "nohttps";
+    public static final String VK_SCOPE_NOTIFY = "notify";
+    public static final String VK_SCOPE_FRIENDS = "friends";
+    public static final String VK_SCOPE_PHOTOS = "photos";
+    public static final String VK_SCOPE_AUDIO = "audio";
+    public static final String VK_SCOPE_VIDEO = "video";
+    public static final String VK_SCOPE_PAGES = "pages";
+    public static final String VK_SCOPE_NOTES = "notes";
+    public static final String VK_SCOPE_STATUS = "status";
+    public static final String VK_SCOPE_MESSAGES = "messages";
+    public static final String VK_SCOPE_WALL = "wall";
+    public static final String VK_SCOPE_ADS = "ads";
+    public static final String VK_SCOPE_OFFLINE = "offline";
+    public static final String VK_SCOPE_DOCS = "docs";
+    public static final String VK_SCOPE_GROUPS = "groups";
+    public static final String VK_SCOPE_NOTIFICATIONS = "notifications";
+    public static final String VK_SCOPE_STATS = "stats";
+    public static final String VK_SCOPE_EMAIL = "email";
+    public static final String VK_SCOPE_MARKET = "market";
+    public static final String VK_SCOPE_NOHTTPS = "nohttps";
 
+    private VkConstants() {
+        super();
+    }
 }
