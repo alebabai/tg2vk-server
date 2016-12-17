70bb6a3e2f3a509e71703bd66e150587f243b5f6
Alexander Babai <aliaksandr.babai@gmail.com>
2016-12-17 20:15:57 +0300

    refactoring(*): remove test properties
    remove duplicated properties file.
    use environment variables for test and prod cases;

--- src/test/java/com/github/alebabai/tg2vk/common/AbstractSpringTest.java
+++ src/test/java/com/github/alebabai/tg2vk/common/AbstractSpringTest.java
@@ -3,13 +3,11 @@ package com.github.alebabai.tg2vk.common;
 import com.github.alebabai.tg2vk.Tg2VkApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {
         Tg2VkApplication.class
 })
-@TestPropertySource("classpath:test.properties")
 public abstract class AbstractSpringTest {
 }
