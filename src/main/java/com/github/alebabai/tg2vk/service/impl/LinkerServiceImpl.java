160f8eaec1cf70442564bf000f22f19048f1ccd7
Alexander Babai <aliaksandr.babai@gmail.com>
2016-12-18 00:00:38 +0300

    feat(properties): add ability to disable vk auto init

--- src/main/java/com/github/alebabai/tg2vk/service/impl/LinkerServiceImpl.java
+++ src/main/java/com/github/alebabai/tg2vk/service/impl/LinkerServiceImpl.java
@@ -3,12 +3,14 @@ package com.github.alebabai.tg2vk.service.impl;
 import com.github.alebabai.tg2vk.domain.User;
 import com.github.alebabai.tg2vk.repository.UserSettingsRepository;
 import com.github.alebabai.tg2vk.service.*;
+import com.github.alebabai.tg2vk.util.constants.EnvConstants;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.vk.api.sdk.client.actors.UserActor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -28,13 +30,16 @@ public class LinkerServiceImpl implements LinkerService {
     private final UserService userService;
     private final UserSettingsRepository settingsRepository;
     private final Map<Integer, AtomicBoolean> daemonStates;
+    private final Environment env;
 
     @Autowired
-    public LinkerServiceImpl(VkService vkService,
+    public LinkerServiceImpl(Environment env,
+                             VkService vkService,
                              TelegramService tgService,
                              TemplateRendererService templateRenderer,
                              UserService userService,
                              UserSettingsRepository settingsRepository) {
+        this.env = env;
         this.vkService = vkService;
         this.tgService = tgService;
         this.templateRenderer = templateRenderer;
@@ -45,7 +50,9 @@ public class LinkerServiceImpl implements LinkerService {
 
     @PostConstruct
     protected void init() {
-        userService.findAllStarted().forEach(this::start);
+        if (env.getProperty(EnvConstants.PROP_VK_AUTO_INIT_POOL, Boolean.TYPE, true)) {
+            userService.findAllStarted().forEach(this::start);
+        }
     }
 
     @Transactional
