76c13e4deeac61d90bd6331e03867895df4bd3d1
Alexander Babai <aliaksandr.babai@gmail.com>
2017-04-15 22:32:54 +0300

    test(service): add PathResolverImpl unit-tests

---
+++ src/test/java/com/github/alebabai/tg2vk/service/impl/PathResolverImplTest.java
@@ -0,0 +1,49 @@
+package com.github.alebabai.tg2vk.service.impl;
+
+import com.github.alebabai.tg2vk.service.PathResolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PathResolverImplTest {
+
+    @Autowired
+    private PathResolver pathResolver;
+
+    @Test
+    public void getClientUrlTest() {
+        final String clientUrl = pathResolver.getClientUrl();
+
+        assertThat(clientUrl, is("https://tg2vk.client.com"));
+    }
+
+    @Test
+    public void getServerUrlTest() {
+        final String serverUrl = pathResolver.getServerUrl();
+
+        assertThat(serverUrl, is("http://tg2vk.server.com:8080"));
+    }
+
+    @Test
+    public void resolveServerUrlTest() {
+        assertThat(pathResolver.resolveServerUrl("/api/users"), is("http://tg2vk.server.com:8080/api/users"));
+        assertThat(pathResolver.resolveServerUrl("////api/users"), is("http://tg2vk.server.com:8080/api/users"));
+        assertThat(pathResolver.resolveServerUrl("api/users"), is("http://tg2vk.server.com:8080/api/users"));
+        assertThat(pathResolver.resolveServerUrl(" api/users"), is("http://tg2vk.server.com:8080/%20api/users"));
+    }
+
+    @Test
+    public void resolveClientUrlTest() {
+        assertThat(pathResolver.resolveClientUrl("/settings"), is("https://tg2vk.client.com/settings"));
+        assertThat(pathResolver.resolveClientUrl("////settings"), is("https://tg2vk.client.com/settings"));
+        assertThat(pathResolver.resolveClientUrl("settings"), is("https://tg2vk.client.com/settings"));
+        assertThat(pathResolver.resolveClientUrl(" settings"), is("https://tg2vk.client.com/%20settings"));
+    }
+}
